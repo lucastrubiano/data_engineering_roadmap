f5515faaa926181241b71a64d81c6fe715472195
lucastrubiano <lucas.trubiano@gmail.com>
2025-11-01 22:24:02 -0300

    chore: reorganizar archivos por tools

--- 0_modern_stack/airflow/airflow.sh
+++
@@ -1,6 +0,0 @@
-# Download the docker-compose file for Airflow
-curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.10.4/docker-compose.yaml'
-
-# Create directories for Airflow
-mkdir -p ./dags ./logs ./plugins ./config
-echo -e "AIRFLOW_UID=$(id -u)" > .airflow.env
--- 0_modern_stack/open_metadata/open_metadata.sh
+++
@@ -1,2 +0,0 @@
-# Download the docker-compose file for OpenMetadata
-curl -sL -o docker-compose.yml https://github.com/open-metadata/OpenMetadata/releases/download/1.6.1-release/docker-compose.yml
